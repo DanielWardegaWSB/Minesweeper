41f9d6be7bcae42217dae2d0b717a8ba5056fa90
Daniel Wardega <danielwardega141196@gmail.com>
2022-09-11 16:31:06 +0200

    game

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+from gui import MainWidget
+
+# Starting the application
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWidget()
+    w.setWindowTitle("Minesweeper")
+    w.show()
+    sys.exit(app.exec_())
